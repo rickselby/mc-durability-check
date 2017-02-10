4e89def5f3686279e6cb4c3e5cd7b7e8f17a0381
Rick Selby <rick@selby-family.co.uk>
2017-02-10 11:19:34 +0000

    First commit, 1.10; spams your chat

---
+++ README.md
@@ -0,0 +1,1 @@
+Minecraft Mod to warn a player when the tool they are using drops below 10% durability left.
---
+++ src/main/java/djomp/durabilitycheck/DurabilityCheck.java
@@ -0,0 +1,46 @@
+package djomp.durabilitycheck;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+/**
+ * Created by rs506 on 10/02/17.
+ */
+@Mod(modid = DurabilityCheck.modId, name = DurabilityCheck.name, version = DurabilityCheck.version, acceptedMinecraftVersions = "[1.10.2]")
+public class DurabilityCheck {
+
+    public static final String modId = "durabilitycheck";
+    public static final String name = "Durability Check";
+    public static final String version = "1.0.0";
+
+    @Mod.Instance(modId)
+    public static DurabilityCheck instance;
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void checkItem(PlayerInteractEvent.LeftClickBlock event) {
+        ItemStack itemStack = event.getItemStack();
+        if (itemStack != null) {
+            if (itemStack.getMaxDamage() != 0) {
+                if (((double) itemStack.getItemDamage() / itemStack.getMaxDamage()) > 0.9) {
+                    event.getEntityPlayer().addChatMessage(
+                            new TextComponentString(
+                                    TextFormatting.RED + "WARNING: " + itemStack.getDisplayName() + " has less than 10% durability left"
+                            )
+                    );
+                }
+            }
+        }
+    }
+
+}
